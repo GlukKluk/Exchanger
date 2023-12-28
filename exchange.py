52dcfa063ba286677c3076b6cae70c7c64b2b80f
GlukKluk <vladrosvladros@gmail.com>
2023-12-28 12:01:14 +0200

    Повна програма

--- exchange.py
+++ exchange.py
@@ -1,12 +1,11 @@
 import requests
 import vikno
 
-# url_nbu = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
-#
-# req_nbu = requests.get(url=url_nbu)
-#
-# price_usd = req_nbu.json()[24]["rate"]
-price_usd = 37.6194
+url_nbu = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+
+req_nbu = requests.get(url=url_nbu)
+
+price_usd = req_nbu.json()[24]["rate"]
 
 
 def backspace():
@@ -21,14 +20,12 @@ def trade():
             pass
 
         elif vikno.pole_1.get():
-            usd_uah = float(vikno.pole_1.get()) * price_usd
+            usd_uah = float(vikno.pole_1.get()) * price_usd * 100 // 1 / 100
             vikno.pole_2.insert(0, usd_uah)
-            print(1)
 
         elif vikno.pole_2.get():
-            usd_uah = float(vikno.pole_2.get()) / price_usd
-            vikno.pole_1.insert(0, usd_uah)
-            print(2)
+            uah_usd = float(vikno.pole_2.get()) / price_usd * 100 // 1 / 100
+            vikno.pole_1.insert(0, uah_usd)
 
     except ValueError:
         pass
